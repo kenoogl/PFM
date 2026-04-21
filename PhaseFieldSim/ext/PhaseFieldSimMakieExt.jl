19b102b92c02a7b56cf5e93a7e4e9ea69e26e44a
Kenji Ono <kenoogl@gmail.com>
2026-04-21 13:13:00 +0900

    feat(pfm-julia-viz): task 2.2 - execution control interface

--- PhaseFieldSim/ext/PhaseFieldSimMakieExt.jl
+++ PhaseFieldSim/ext/PhaseFieldSimMakieExt.jl
@@ -10,17 +10,17 @@ using Printf
 
 Simulationオブジェクトを受け取り、GLMakieを用いた可視化ダッシュボードを表示します。
 濃度場 c2, c3 のヒートマップ、およびステップ数とシミュレーション時間を表示します。
+実行制御用のボタン（Start/Stop, Step, Reset）を備えています。
 """
 function PhaseFieldSim.visualize(sim::Simulation)
     # Figureの作成
-    fig = Figure(size = (1000, 600))
+    fig = Figure(size = (1000, 700))
     
     # レイアウト設定: 濃度場 c2 と c3 の Axis を作成
     ax2 = Axis(fig[1, 1], title = "Concentration c2", aspect = DataAspect())
     ax3 = Axis(fig[1, 2], title = "Concentration c3", aspect = DataAspect())
     
     # データの Observable 化
-    # Array() を介することで、ParallelStencil の配列から標準の CPU 配列に変換して Makie に渡します
     obs_c2 = Observable(Array(sim.c2))
     obs_c3 = Observable(Array(sim.c3))
     
@@ -33,7 +33,6 @@ function PhaseFieldSim.visualize(sim::Simulation)
     Colorbar(fig[1, 4], hm3, label = "c3")
 
     # ステップ数と時間の表示用 Observable
-    # これらは将来的にシミュレーションループから更新されることを想定しています
     obs_step = Observable(0)
     obs_time = Observable(0.0)
     
@@ -44,6 +43,61 @@ function PhaseFieldSim.visualize(sim::Simulation)
     
     # 上部に情報のラベルを配置
     Label(fig[0, :], label_text, fontsize = 20, font = :bold, tellwidth = false)
+    
+    # --- 実行制御インターフェース ---
+    
+    # 実行状態の管理
+    is_running = Observable(false)
+    
+    # ボタンの配置用グリッド
+    controls = fig[2, :] = GridLayout(tellwidth = false)
+    
+    btn_run = Button(controls[1, 1], label = lift(x -> x ? "Stop" : "Start", is_running))
+    btn_step = Button(controls[1, 2], label = "Step")
+    btn_reset = Button(controls[1, 3], label = "Reset")
+    
+    # Start/Stop ボタンのロジック
+    on(btn_run.clicks) do _
+        is_running[] = !is_running[]
+    end
+    
+    # Step ボタンのロジック
+    on(btn_step.clicks) do _
+        if !is_running[]
+            PhaseFieldSim.step!(sim)
+            obs_c2[] = Array(sim.c2)
+            obs_c3[] = Array(sim.c3)
+            obs_step[] += 1
+            obs_time[] += sim.dt
+        end
+    end
+    
+    # Reset ボタンのロジック
+    on(btn_reset.clicks) do _
+        is_running[] = false
+        PhaseFieldSim.initialize_noise!(sim)
+        obs_c2[] = Array(sim.c2)
+        obs_c3[] = Array(sim.c3)
+        obs_step[] = 0
+        obs_time[] = 0.0
+    end
+    
+    # シミュレーションループ
+    # @async を使用して GUI スレッドをブロックせずに実行
+    @async while isopen(fig.scene)
+        if is_running[]
+            PhaseFieldSim.step!(sim)
+            obs_c2[] = Array(sim.c2)
+            obs_c3[] = Array(sim.c3)
+            obs_step[] += 1
+            obs_time[] += sim.dt
+            # 描画の更新タイミングを確保
+            yield()
+        else
+            # 停止中は負荷を下げる
+            sleep(0.1)
+        end
+    end
     
     # レイアウトの比率調整
     colsize!(fig.layout, 1, Relative(0.45))
